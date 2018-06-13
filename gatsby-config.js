ee769b0281d4db07e8ee8f75973e779eb139c32f
Hai Nguyen <nghai.dev@gmail.com>
2018-06-14 00:27:48 +0700

    Add ESLint & Gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,9 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-react-next',
+  ],
 }
